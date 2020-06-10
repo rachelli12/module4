0d31cb10dad9c3bb0678b4b79b8757f4dfd9388f
rachelli12 <65979017+rachelli12@users.noreply.github.com>
2020-06-10 01:04:20 -0500

    Add files via upload

---
+++ test_validation_with_try.py
@@ -0,0 +1,18 @@
+import unittest
+import unittest.mock as mock
+from input_validation import validation_with_try as vwt
+
+class AverageTestCase(unittest.TestCase):
+    def test_average(self):
+        with mock.patch('builtins.input', side_effect=[85,90,95]):
+            assert vwt.average(85, 90, 95) == 90
+    def test_average_exceptiont(self):
+        with self.assertRaises(ValueError):
+            vwt.average(-90, 89, 78)
+try:
+    num = int("string")
+except ValueError:
+    raise ValueError("ValueError exception thrown")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ validation_with_try.py
@@ -0,0 +1,19 @@
+NUMBER_TESTS = 3
+def average():
+    #Get input for scores
+    average_scores = (score1 + score2 + score3) / 3
+    print('average: ', (score1 + score2 + score3) / 3)
+    # declare variables. use score1, score2, score3 in calculation
+    return average_scores
+
+def average(score1, score2, score3):
+    NUMBER_TESTS = 3
+    return float((score1 + score2 + score3)/NUMBER_TESTS)
+
+if __name__ == '__main__':
+    score1 = int(input('score 1: '))
+    score2 = int(input('score 2: '))
+    score3 = int(input('score 3: '))
+    #Calculate average scores
+    average_scores = average(score1, score2, score3)
+    print(average_scores)
