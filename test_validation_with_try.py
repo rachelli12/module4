ea460af3ec0dff8b65f8856cb49322a37d8aead7
rachelli <brli@dmacc.edu>
2020-06-10 19:48:01 -0500

    test_validation_with_try.py

--- test_validation_with_try.py
+++ test_validation_with_try.py
@@ -9,6 +9,9 @@ class AverageTestCase(unittest.TestCase):
     def test_average_exception(self):
         with self.assertRaises(ValueError):
             vwt.average(-90, 89, 78)
+    def test_average_exception(self):
+        with self.assertRaises(ValueError):
+            vwt.average(90, -89, 78)
 
 if __name__ == '__main__':
     unittest.main()
