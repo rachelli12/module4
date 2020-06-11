be0a8273f38c68d57e3ca98a70ee316963c360b9
rachelli <brli@dmacc.edu>
2020-06-10 19:40:38 -0500

    test_coupon_calculations.py

--- test_coupon_calculations.py
+++ test_coupon_calculations.py
@@ -1,22 +1,20 @@
-"""
+""""
 Program
 """
 import unittest
-import unittest.mock as mock
 import coupon_calculations as cc
 
 class MyTestCase(unittest.TestCase):
-    #def calculate_price(price, cash_coupon, percent_coupon):
+    #def total = calculate_total(calculate_shipping(calculate_price(price, cash_coupon, percent_coupon)))
     def test_price_under_ten(self):
         value_under_ten = 8.99 #shipping $5.95
-        self.assertAlmostEqual(cc.calculate_price(value_under_ten, 5, 10), 10.60, places=4)
-        self.assertAlmostEqual(cc.calculate_price(value_under_ten, 5, 15), 10.40, places=4)
-        self.assertAlmostEqual(cc.calculate_price(value_under_ten, 5, 20), 10.20, places=4)
-        self.assertAlmostEqual(cc.calculate_price(value_under_ten, 10, 10), 6.10, places=4)
-        self.assertAlmostEqual(cc.calculate_price(value_under_ten, 10, 15), 6.15, places=4)
-        self.assertAlmostEqual(cc.calculate_price(value_under_ten, 10, 20), 6.20, places=4)
+        self.assertAlmostEqual(cc.cost, 10.11, places=4)
+        self.assertAlmostEqual(cc.cost, 9.90, places=4)
+        self.assertAlmostEqual(cc.cost, 9.69, places=4)
+        self.assertAlmostEqual(cc.cost, 5.34, places=4)
+        self.assertAlmostEqual(cc.cost, 5.40, places=4)
+        self.assertAlmostEqual(cc.cost, 5.45, places=4)
 
 
 if __name__ == '__main__':
     unittest.main()
-
