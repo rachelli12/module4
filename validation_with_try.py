8ab52905b03d7a3d7f10d6de17068c49b71b17b4
rachelli <brli@dmacc.edu>
2020-06-10 22:07:26 -0500

    validation_with_try.py

--- validation_with_try.py
+++ validation_with_try.py
@@ -32,7 +32,7 @@ score2 = int(input('score 2: '))
 score3 = int(input('score 3: '))
 #Calculate average scores
 average_scores = average(score1, score2, score3)
-print(average_sco
+print(average_scores)
 
 if __name__ == '__main__':
     pass
