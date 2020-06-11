5d14d172c48f1c317f5623f8975176db1098371e
rachelli <brli@dmacc.edu>
2020-06-10 19:45:22 -0500

    validation_with_try.py

--- validation_with_try.py
+++ validation_with_try.py
@@ -1,24 +1,24 @@
 NUMBER_TESTS = 3
 def average():
     #Get input for scores
-    average_scores = (score1 + score2 + score3) / 3
-    print('average: ', (score1 + score2 + score3) / 3)
     # declare variables. use score1, score2, score3 in calculation
     return average_scores
 
 def average(score1, score2, score3):
     NUMBER_TESTS = 3
+    if 0<score1<=100:
+        print("score1 is within range.")
+    else:
+        print("score1 must be positive.")
+        raise ValueError
     return float((score1 + score2 + score3)/NUMBER_TESTS)
 
-
+score1 = int(input('score 1: '))
+score2 = int(input('score 2: '))
+score3 = int(input('score 3: '))
+#Calculate average scores
+average_scores = average(score1, score2, score3)
+print(average_scores)
 
 if __name__ == '__main__':
-    score1 = int(input('score 1: '))
-    score2 = int(input('score 2: '))
-    score3 = int(input('score 3: '))
-    #Calculate average scores
-    average_scores = average(score1, score2, score3)
-    print(average_scores)
-
-    if score1 < 0:
-        raise ValueError('score1 should not be less than 0!')
+    pass
