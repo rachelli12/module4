cbbf336fed541c7591d82c0b0bae9c058a98e340
rachelli <brli@dmacc.edu>
2020-06-10 21:34:55 -0500

    test_coupon_calculations.py

--- test_coupon_calculations.py
+++ test_coupon_calculations.py
@@ -5,16 +5,22 @@ import unittest
 import coupon_calculations as cc
 
 class MyTestCase(unittest.TestCase):
-    #def total = calculate_total(calculate_shipping(calculate_price(price, cash_coupon, percent_coupon)))
+    #def calculate_price(value_under_ten, cash_coupon, percent_coupon)
     def test_price_under_ten(self):
-        value_under_ten = 8.99 #shipping $5.95
-        self.assertAlmostEqual(cc.cost, 10.11, places=4)
-        self.assertAlmostEqual(cc.cost, 9.90, places=4)
-        self.assertAlmostEqual(cc.cost, 9.69, places=4)
-        self.assertAlmostEqual(cc.cost, 5.34, places=4)
-        self.assertAlmostEqual(cc.cost, 5.40, places=4)
-        self.assertAlmostEqual(cc.cost, 5.45, places=4)
-
+        value_under_ten = 8.99
+        shipping = 5.95
+        self.assertAlmostEqual(((cc.calculate_price(value_under_ten,5,10) + shipping)*1.06), 10.11, places=2)
+        self.assertAlmostEqual(((cc.calculate_price(value_under_ten, 5, 15) + shipping)*1.06), 9.90, places=2)
+        self.assertAlmostEqual(((cc.calculate_price(value_under_ten, 5, 20) + shipping)*1.06), 9.69, places=2)
+    def test_price_under_between_ten_thirty(self):
+        value_under_between_ten_thirty = 18.99
+        shipping = 7.95
+        self.assertAlmostEqual(((cc.calculate_price(value_under_between_ten_thirty, 5,10) + shipping)*1.06), 21.77, places=2)
+        self.assertAlmostEqual(((cc.calculate_price(value_under_between_ten_thirty, 5, 15) + shipping)*1.06), 21.03, places=2)
+        self.assertAlmostEqual(((cc.calculate_price(value_under_between_ten_thirty, 5, 20) + shipping)*1.06), 20.29, places=2)
+        self.assertAlmostEqual(((cc.calculate_price(value_under_between_ten_thirty, 10,10) + shipping)*1.06), 17.00, places=2)
+        self.assertAlmostEqual(((cc.calculate_price(value_under_between_ten_thirty, 10, 15) + shipping)*1.06), 16.53, places=2)
+        self.assertAlmostEqual(((cc.calculate_price(value_under_between_ten_thirty, 10, 20) + shipping)*1.06), 16.05, places=2)
 
 if __name__ == '__main__':
     unittest.main()
